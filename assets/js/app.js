b670833857eb4a689c47597184968aa4fff7d71a
Steve Harold <stehar14@yahoo.com>
2017-12-12 21:33:11 -0500

    JS finished. Have to style, make separate pages

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,21 @@
+// GifTastic
+// Written by: Steve Harold
+// Created: 12/11/2017
+// Last Updated: 12/12/2017
+
+// Function used to turn user input into title case //
+function toTitleCase(str) {
+  return str.replace(/(?:^|\s)\w/g, function(match) {
+      return match.toUpperCase();
+  });
+}
+
+// On click function for initial and dynamically created buttons
+// Populates the #images div with 10 gifs related to the phrase on the button pressed
+
 $("#buttons").on("click", ".button", function() {
   $("#images").empty();
-  //set variable storing the data-person of the button you clicked
+  //set variable storing the data-name of the button you clicked
   var person = $(this).attr("data-name");
   //set variable to giphy api with data-person search
   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
@@ -10,32 +25,37 @@ $("#buttons").on("click", ".button", function() {
       url: queryURL,
       method: "GET"
   })
-//when get method finishes
+  //when get method finishes
   .done(function(response) {
   //store object in variable
     var results = response.data;
-    //iterate through the array 
+  //iterate through the array 
     for (var i = 0; i < results.length; i++) {
-      //create new div with class item for each result
-      var gifDiv = $("<div>");
-      //store rating for each result in variable
+  //create new div with class item for each result
+      var gifDiv = $("<div class='col-md-4'>");
+  //store rating for each result in variable
       var rating = results[i].rating;
-      //create paragraph element to display rating on screen
+  //create paragraph element to display rating on screen
       var p = $("<p>").text("Rating: " + rating);
-      //create img element to show each gif and then set the source
-      var personImage = $("<img class='item' data-still='" + results[i].images.fixed_height_still.url + "' data-animate='" +results[i].images.fixed_height.url + "' data-state='still'>");
-      personImage.attr("src", results[i].images.fixed_height_still.url);
-      // add parapgraphs and imgs to beginning of each newly created div
+  //create img element to show each gif and then set the source
+      var personImage = $("<img>");
+      personImage.attr("class", "item");
+      personImage.attr("data-still", results[i].images.fixed_width_still.url);
+      personImage.attr("data-animate", results[i].images.fixed_width.url);
+      personImage.attr("data-state", "still");
+      personImage.attr("src", results[i].images.fixed_width_still.url);
+  // add parapgraphs and imgs to beginning of each newly created div
       gifDiv.prepend(p);
       gifDiv.prepend(personImage);
-      // add new divs to #gifs=appear-here div
+  // add new divs to #images div
       $("#images").prepend(gifDiv);
     }
   });
 });
 
+// On click function for dynamically created GIFs
 $("#images").on("click", ".item", function() {
-      // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+  // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
   var state = $(this).attr("data-state");
   // If the clicked image's state is still, update its src attribute to what its data-animate value is.
   // Then, set the image's data-state to animate
@@ -49,13 +69,18 @@ $("#images").on("click", ".item", function() {
   }
 });
 
+//On click function for user input submission button
 $("#addButton").on("click", function() {
   event.preventDefault();
-  var phrase = $("#addPhrase").val().trim();
-  var newButton = $("<button>" + phrase + "</button>");
-  newButton.attr("id", phrase);
-  newButton.attr("class","button");
-  newButton.attr("data-name", phrase);
-  newButton.appendTo("#buttons");
-
+  if ($("#addPhrase").val().trim() === "") {
+    alert("That's not a valid input!");
+  } else{
+    var phrase = toTitleCase($("#addPhrase").val().trim());
+    var newButton = $("<button>" + phrase + "</button>");
+    newButton.attr("id", phrase);
+    newButton.attr("class","button");
+    newButton.attr("data-name", phrase);
+    newButton.appendTo("#buttons");
+    $("#addPhrase").val("");
+  }
 });
